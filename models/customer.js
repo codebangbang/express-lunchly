e82ea1958254b416cacfd012a7e70b2b507d445a
ryanfair <ryfair@gmail.com>
2024-04-02 17:49:27 -0400

    Parts 1 - 6 Complete!

--- models/customer.js
+++ models/customer.js
@@ -53,6 +53,11 @@ class Customer {
     return new Customer(customer);
   }
 
+  /** get the full name for a customer. */
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** get all reservations for this customer. */
 
   async getReservations() {
